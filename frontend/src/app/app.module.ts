45d67219a233549b0a38e7767af01ee35434e435
sudhir8581raj <sudhir8581raj@gmail.com>
2021-01-24 23:16:24 +0530

    created components like login,singup,password reset

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './@public/navbar/navbar.component';
+import { LoginComponent } from './@public/login/login.component';
+import { SingupComponent } from './@public/singup/singup.component';
+import { RequestResetComponent } from './@public/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './@public/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SingupComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule
