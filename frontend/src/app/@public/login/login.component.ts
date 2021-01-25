acd2eff755b3f8f0f12d09e4589a5e0bc1b708f3
sudhir8581raj <sudhir8581raj@gmail.com>
2021-01-25 22:37:32 +0530

    fix bug

--- frontend/src/app/@public/login/login.component.ts
+++ frontend/src/app/@public/login/login.component.ts
@@ -19,9 +19,10 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(){
-    this.http.post('http://127.0.0.1:8000/api/auth/login',this.form).subscribe((response)=>{
+    this.http.post('http://127.0.0.1:8000/api/login',this.form).subscribe((response)=>{
       console.log(response);
     },(error)=>{
+      console.log(error)
       this.handleError(error);
     });
   }
--- frontend/src/app/@public/signup/signup.component.ts
+++ frontend/src/app/@public/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public form:any = {
+    name : null,
+    email : null,
+    password : null,
+    password_confirmation : null
+  };
+  public error: any = [];
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.http.post('http://127.0.0.1:8000/api/signup',this.form).subscribe((response)=>{
+      console.log(response);
+    },(error)=>{
+      console.log(error)
+      this.handleError(error);
+    });
+  }
+  handleError(error){
+    this.error = error.error.errors;
   }
 
 }
